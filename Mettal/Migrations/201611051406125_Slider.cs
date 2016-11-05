1f3a25304a92cdf9a07085b8847b79ffad5a1d3f
Artur Mezhidov <arturrasulovich@gmail.com>
2016-11-05 22:58:42 +0300

    Register & Login

---
+++ Mettal/Migrations/201611051406125_Slider.cs
@@ -0,0 +1,29 @@
+namespace Mettal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Slider : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Slides",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ImageUrl = c.String(),
+                        Title = c.String(),
+                        IsHidden = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Slides");
+        }
+    }
+}
